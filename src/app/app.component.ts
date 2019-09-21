a54e43c7eedbc6a2269b1e3b1991ebd14beb81e8
Xobot <peter.karamitsos@gmail.com>
2019-09-21 11:07:58 -0400

    Fixed some typos. Added more comments. Made code neater.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
+
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+
+// Metadata decorator:
 @Component({
-  selector: 'pm-root',
-  templateUrl: './app.component.html',
+  selector: 'pm-root', // The convention for naming these is to add a prefix for your app. In this case, 'pm' for 'Product Management'
+  template: `<div class='welcome-title'><h1>{{pageTitle}}</h1>
+        <div>
+          Components!!
+        </div></div>`,
   styleUrls: ['./app.component.css']
 })
+
+// Class:
+// By convention, the root component is called AppComponent
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle: string = 'Acme Product Manager'; // type not required explicitly for 'title'. It is inferred from the string literal.
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+@NgModule({ // Decortor to define a class as a Module
+  // Defines which components belong to this module:
+  declarations: [AppComponent],
+  // Defines the external modules that we want to have available to all of the components that belong to this angular module:
+  imports: [BrowserModule], // Every browser module must import this
+  // Defines the startup Component. This component should contain the selector we use in index.html
+  // In this cas pm-root, which is defined in AppComponent
   bootstrap: [AppComponent]
 })
 export class AppModule { }
