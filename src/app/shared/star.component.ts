db2a93aec794400c782419b77df662b4903594fa
Xobot <peter.karamitsos@gmail.com>
2019-10-12 02:45:58 -0400

    Testing netlify deploy

--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -5,7 +5,7 @@ import { Component, OnChanges, Input, Output, EventEmitter } from '@angular/core
 @Component({
     selector: 'pm-star',
     templateUrl: './star.component.html',
-    styleUrls: ['./star.component.css']
+    styleUrls: ['./star.component.css'],
 })
 
 export class StarComponent implements OnChanges {
